f58404deb551e809899e5cedbe85e60e2e3e5d67
Ethan Harvey <harvey.ethan@ufl.edu>
2022-03-07 01:42:14 -0500

    Adding numbers

---
+++ src/Lab6Add.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Lab6Add {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int num1 = scnr.nextInt();
+        int num2 = scnr.nextInt();
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+    }
+}
