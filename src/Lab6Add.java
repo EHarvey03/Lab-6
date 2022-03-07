49cd1a1119586e835ecd358c57e4c4ca3c6d92f0
Ethan Harvey <harvey.ethan@ufl.edu>
2022-03-07 01:57:17 -0500

    Add and Subtract numbers

--- src/Lab6Add.java
+++ src/Lab6Add.java
@@ -3,10 +3,21 @@ import java.util.Scanner;
 public class Lab6Add {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
+        System.out.println("1. Add\n2. Subtract");
+        int MenuChoice = scnr.nextInt();
+        System.out.println("Enter two numbers:");
         int num1 = scnr.nextInt();
         int num2 = scnr.nextInt();
-        int sum = num1 + num2;
 
-        System.out.println(sum);
+        if (MenuChoice == 1) {
+            int sum = num1 + num2;
+            System.out.println(sum);
+        }
+        if (MenuChoice == 2) {
+            int difference = num1 - num2;
+            System.out.println(difference);
+        }
+
+
     }
 }
